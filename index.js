b118cb48a6cf5a3add23989e39f7cb250b13c0f2
karthikkbdl4o <109502155+karthikkbdl4o@users.noreply.github.com>
2022-10-14 17:39:45 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,47 @@
+//import Express
+const express = require("express");
+
+//Create Express Create
+const app = express();
+
+//GET - To Read
+//POST - To Create
+//DELETE - To Delete
+//PUT - To Update
+
+// CRUD
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Index",
+  });
+});
+
+app.get("/example", (req, res) => {
+  res.json({
+    message: "Get Method",
+  });
+});
+
+app.post("/example", (req, res) => {
+  res.json({
+    message: "Post Method",
+  });
+});
+
+app.put("/example", (req, res) => {
+  res.json({
+    message: "Put Method",
+  });
+});
+
+app.delete("/example", (req, res) => {
+  res.json({
+    message: "Delete Method",
+  });
+});
+
+// Listen To Port
+app.listen(3000, () => {
+  console.log("Express Server Started");
+});
